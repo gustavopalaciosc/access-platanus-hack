74ff892e7b19ad13b4b3f9ac9e3136822732e857
gustavopalaciosc <gpalaciosc@uc.cl>
2024-11-10 23:45:38 -0300

    fix: minor change in participants page

--- src/app/participants/page.tsx
+++ src/app/participants/page.tsx
@@ -27,28 +27,27 @@ export default function ParticipantsPage() {
         fetchParticipants();
     }, []);
 
-    const verifiedParticipants = participants.filter((participant) => participant.is_verified);
+    const noVerifiedParticipants = participants.filter((participant) => !participant.is_verified);
 
     return (
         <div className="min-h-screen p-6">
-            <h1 className="text-3xl font-semibold text-center mb-6">Participantes Verificados</h1>
+            <h1 className="text-3xl font-semibold text-center mb-6">Participantes No Validados</h1>
 
            
             {loading && <p className="text-center text-gray-500">Cargando participantes...</p>}
 
             {error && <p className="text-center text-red-600">{error}</p>}
 
-            {verifiedParticipants.length === 0 && !loading && !error ? (
-                <p className="text-center text-gray-500">Aún no hay participantes verificados.</p>
+            {noVerifiedParticipants.length === 0 && !loading && !error ? (
+                <p className="text-center text-gray-500">No quedan participantes por validar.</p>
             ) : (
         <div className="space-y-4 max-w-md mx-auto h-96 bg-[#c2c2c2] overflow-y-auto p-4 rounded-md">
-            {verifiedParticipants.map((participant) => (
+            {noVerifiedParticipants.map((participant) => (
             <div
                 key={participant.id}
                 className="p-4 bg-yellow-500 rounded-lg shadow-md transition-all"
             >
             <p className="text-xl font-semibold">{participant.name} {participant.last_name}</p>
-            <p className="text-sm text-gray-500">Entrada: {new Date(participant.entry_time).toLocaleString()}</p>
         </div>
     ))}
     </div>
